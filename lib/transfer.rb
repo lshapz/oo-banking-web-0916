70ffb03dcdf07a16bae8f1d2029caa1a7351c59d
lshapz <laurashapiro01@gmail.com>
2016-09-30 16:10:16 -0400

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,51 @@
+require 'Pry'
 class Transfer
-  # your code here
+  
+  attr_accessor :sender, :receiver, :status, :amount 
+
+  def initialize(sender, receiver, amount)
+   @sender = sender
+   @receiver = receiver
+   @status  = "pending"
+   @amount = amount  
+  end 
+
+  def valid?
+    (@sender.valid? && @receiver.valid?) ? true : false
+  end
+
+  def execute_transaction
+    if @sender.valid? != true || @sender.balance <= @amount
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    elsif @status != "complete"
+      @status = "complete"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+    end 
+  end
+
+  # def execute_transaction
+  #   if @status == "pending" 
+  #     if 
+  #       @status = "rejected"
+  #       return "Transaction rejected. Please check your account balance."
+  #     elsif @sender.valid? == true
+  #       @status = "complete"
+  #       @sender.balance -= @amount
+  #       @receiver.balance += @amount
+  #     end
+  #   else 
+  #     nil
+  #   end 
+  # end
+
+  def reverse_transfer
+    if @status == "complete"
+      @receiver.balance -= @amount
+      @sender.balance += @amount
+    end 
+    @status = "reversed"
+  end
+
 end
--- spec/banking_spec.rb
+++ spec/banking_spec.rb
@@ -1,61 +1,29 @@
-describe "BankAccount" do
+class BankAccount
 
-  let(:avi) { BankAccount.new("Avi") }
+  attr_accessor :balance, :status
+  attr_reader :name
 
-  describe 'BankAccount' do
-    describe "#initialize" do
-      it "can initialize a Bank Account" do
-        expect(avi).to be_a(BankAccount)
-      end
+  def initialize(name, balance = 1000, status = 'open')
+    @name = name
+    @balance = balance
+    @status = status
+  end 
 
-      it "initializes with a name" do
-        expect(avi.name).to eq("Avi")
-      end
-
-      it "always initializes with balance of 1000" do
-        expect(avi.balance).to eq(1000)
-      end
-
-      it "always initializes with a status of 'open'" do
-        expect(avi.status).to eq("open")
-      end
-
-      it "can't change its name" do
-        expect { avi.name = "Bob" }.to raise_error
-      end
-    end
+  def deposit(amount)
+    @balance += amount
   end
 
-  describe '#deposit' do
-    it "can deposit money into its account" do
-      expect(avi.balance).to eq(1000)
-      avi.deposit(1000)
-      expect(avi.balance).to eq(2000)
-    end
+  def display_balance
+    "Your balance is $#{self.balance}."
   end
 
-  describe '#display_balance' do
-    it "can display its balance" do
-      expect(avi.display_balance).to eq("Your balance is $#{avi.balance}.")
-    end
+  def valid?
+    self.status == 'open' && self.balance > 0 ? true : false
+  end 
+
+  def close_account
+    self.status = 'closed'
   end
 
-  describe '#valid?' do
-    it "is valid with an open status and a balance greater than 0" do
-      @broke = BankAccount.new("Kat Dennings")
-      @broke.balance = 0
-      @closed = BankAccount.new("Beth Behrs")
-      @closed.status = "closed"
-      expect(avi.valid?).to eq(true)
-      expect(@broke.valid?).to eq(false)
-      expect(@closed.valid?).to eq(false)
-    end
-  end
 
-  describe '#close_account' do
-    it "can close its account" do
-      avi.close_account
-      expect(avi.status).to eq("closed")
-    end
-  end
 end
